cf71bfdcf5dff5258ebccd71335d7a6fc9fb1d4b
Rick Stroobosscher <Rick@ricks-mbp-2.lan>
2020-01-03 23:45:26 -0500

    Added support for printing sequences.

--- core/attribute.go
+++ core/attribute.go
@@ -14,17 +14,39 @@ type Attribute struct {
 
 // String returns attribute as a string
 func (attribute *Attribute) String() string {
-	s := fmt.Sprintf("tag=(%04X,%04X) vr=%s offset=%d length=%d", attribute.group, attribute.element, attribute.vr, attribute.offset, attribute.length)
+	return attributeToString(attribute, "")
+}
+
+func attributeToString(attribute *Attribute, prefix string) string {
+	s := fmt.Sprintf("%stag=(%04X,%04X) vr=%s off=%d len=%d", prefix, attribute.group, attribute.element, attribute.vr, attribute.offset, attribute.length)
 	switch attribute.vr {
 	case "AE", "AS", "CS", "DA", "DT", "LO", "SH", "TM", "UC", "UI", "UR", "LT", "ST", "UT", "PN":
-		s += fmt.Sprintf(" value=%q", attribute.value)
+		s += fmt.Sprintf(" value=%q\n", attribute.value)
 	case "DS", "IS", "FD", "OD", "FL", "OF", "SS", "US", "SL", "UL", "SV", "UV":
-		s += fmt.Sprintf(" value=%v", attribute.value)
-	case "AT", "OB", "OL", "OV", "OW", "UN", "SQ":
+		s += fmt.Sprintf(" value=%v\n", attribute.value)
+	case "SQ":
+		s += "\n" + sequenceToString(attribute.value.(*Sequence), prefix)
+	case "AT", "OB", "OL", "OV", "OW", "UN":
+		s += "\n"
 	default:
 	}
-	return s + "\n"
+	return s
+}
 
+func sequenceToString(sequence *Sequence, prefix string) string {
+	s := ""
+	for i, item := 1, sequence.objects.Front(); item != nil; i, item = i+1, item.Next() {
+		s += itemToString(item.Value.(*Object), fmt.Sprintf("%s%d>", prefix, i))
+	}
+	return s
+}
+
+func itemToString(object *Object, prefix string) string {
+	s := ""
+	for item := object.attributes.Front(); item != nil; item = item.Next() {
+		s += attributeToString(item.Value.(*Attribute), prefix)
+	}
+	return s
 }
 
 // AsLong returns attribute value as a long
