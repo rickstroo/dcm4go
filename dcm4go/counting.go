bf81466ddd26839d723a097f6487eeeac7b35d4c
Rick Stroobosscher <Rick@ricks-mbp-2.lan>
2020-01-06 23:55:42 -0500

    Adding support for counting readers and limited counting readers.

---
+++ dcm4go/counting.go
@@ -0,0 +1,47 @@
+package dcm4go
+
+import "io"
+
+// CountingReader wraps a reader and counts the bytes read
+type CountingReader struct {
+	bytesRead int
+	reader    io.Reader
+}
+
+// newCountingReader returns a new counting reader
+func newCountingReader(reader io.Reader) *CountingReader {
+	return &CountingReader{0, reader}
+}
+
+// Read calls underlying reader and counts the bytes
+func (countingReader *CountingReader) Read(buf []byte) (int, error) {
+	num, err := countingReader.reader.Read(buf)
+	countingReader.bytesRead += num
+	return num, err
+}
+
+// BytesRead returns the number of bytes read
+func (countingReader *CountingReader) BytesRead() int {
+	return countingReader.bytesRead
+}
+
+// LimitedCountingReader wraps a limited reader and provides access to bytes read
+type LimitedCountingReader struct {
+	countingReader *CountingReader
+	limitReader    io.Reader
+}
+
+// newLimitedReader returns a new limited counting reader
+func newLimitedCountingReader(countingReader *CountingReader, length int) *LimitedCountingReader {
+	return &LimitedCountingReader{countingReader, io.LimitReader(countingReader, int64(length))}
+}
+
+// Read reads bytes from the underling limited reader
+func (limitedCountingReader *LimitedCountingReader) Read(buf []byte) (int, error) {
+	return limitedCountingReader.limitReader.Read(buf)
+}
+
+// BytesRead returns the number of bytes read by the underlying counting reader
+func (limitedCountingReader *LimitedCountingReader) BytesRead() int {
+	return limitedCountingReader.countingReader.BytesRead()
+}
