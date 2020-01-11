4da37eb57a5d218c7ef1e04bf624275ff745f307
Rick Stroobosscher <Rick@ricks-mbp-2.lan>
2020-01-11 00:20:41 -0500

    Completed writing unit tets for attribute.

--- dcm4go/attribute_test.go
+++ dcm4go/attribute_test.go
@@ -13,6 +13,13 @@ func testLongEquals(a, b uint32) error {
 	return nil
 }
 
+func TestAttributeToString(t *testing.T) {
+	attribute := &Attribute{FileMetaInformationGroupLengthTag, "UL", 4, 0, []uint32{0x04}}
+	if err := testStringEquals(attribute.String(), "tag=(0002,0000) vr=UL off=0 len=4 value=[4]\n"); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestAttributeAsLong(t *testing.T) {
 	attribute := &Attribute{FileMetaInformationGroupLengthTag, "UL", 4, 0, []uint32{0x04}}
 	value, err := attribute.asLong(0)
@@ -32,6 +39,14 @@ func TestAttributeAsLongIndexOutOfBounds(t *testing.T) {
 	}
 }
 
+func TestAttributeAsLongWrongType(t *testing.T) {
+	attribute := &Attribute{FileMetaInformationGroupLengthTag, "UL", 4, 0, []uint16{0x04}}
+	_, err := attribute.asLong(0)
+	if !errors.Is(err, ErrWrongType) {
+		t.Error(err)
+	}
+}
+
 func testStringEquals(a, b string) error {
 	if a != b {
 		return fmt.Errorf("expected '%s', was '%s'", a, b)
@@ -46,6 +61,22 @@ func TestAttributeAsString(t *testing.T) {
 		t.Error(err)
 	}
 	if err := testStringEquals(value, "CT"); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestAttributeAsStringIndexOutOfBounds(t *testing.T) {
+	attribute := &Attribute{ModalityTag, "CS", 4, 0, []string{"CT"}}
+	_, err := attribute.asString(1)
+	if !errors.Is(err, ErrIndexOutOfBounds) {
+		t.Error(err)
+	}
+}
+
+func TestAttributeAsStringWrongType(t *testing.T) {
+	attribute := &Attribute{ModalityTag, "CS", 4, 0, []byte{0x01}}
+	_, err := attribute.asString(0)
+	if !errors.Is(err, ErrWrongType) {
 		t.Error(err)
 	}
 }
