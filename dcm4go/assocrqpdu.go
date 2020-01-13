dba4f38d80b8a1d58b0d0cacf9474fdb78d020bb
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-01-13 14:44:06 -0500

    Cleaned up String methods.  Added support for user information.

--- dcm4go/assocrqpdu.go
+++ dcm4go/assocrqpdu.go
@@ -19,37 +19,6 @@ type AssocRQPDU struct {
 	userInfo       *UserInfo
 }
 
-// String returns a string representation of a AssocRQPDU
-func (pdu *AssocRQPDU) String() string {
-
-	// format the simple attribute of the pdu
-	s := fmt.Sprintf("protocol: %v, calledAET: %q, callingAET: %q, appContextName: %q, presContexts: [", pdu.protocol, strings.TrimSpace(pdu.calledAETitle), strings.TrimSpace(pdu.callingAETitle), pdu.appContextName)
-
-	// print the list of presentation contexts
-	for item := pdu.presContexts.Front(); item != nil; item = item.Next() {
-		presContext := item.Value.(*PresContext)
-		s += fmt.Sprintf("{id: %d, abstractSyntax: %q, transferSyntaxes: [", presContext.id, presContext.abstractSyntax)
-
-		// print the list of transfer syntaxes
-		for item := presContext.transferSyntaxes.Front(); item != nil; item = item.Next() {
-			transferSyntax := item.Value.(string)
-			s += fmt.Sprintf("%q,", transferSyntax)
-		}
-
-		// trim the trailing comma separating transfer syntaxes and add the enclosing bracket
-		s = strings.TrimSuffix(s, ",") + "]"
-
-		// add a closing parenthesis and comma for this presentation context
-		s += "},"
-	}
-
-	// remove the trainling comma separating presentation contexts and add the enclosing bracket
-	s = strings.TrimSuffix(s, ",") + "]"
-
-	// return the fully constructed string
-	return s
-}
-
 // PresContext represents a presentation context
 type PresContext struct {
 	id               byte
@@ -59,6 +28,68 @@ type PresContext struct {
 
 // UserInfo represents user information
 type UserInfo struct {
+	maxLenReceived     uint32
+	implClassUID       string
+	implVersionName    string
+	maxNumOpsInvoked   uint16
+	maxNumOpsPerformed uint16
+}
+
+// String returns a string representation of a AssocRQPDU
+func (pdu *AssocRQPDU) String() string {
+
+	// format the simple attribute of the pdu
+	s := fmt.Sprintf(
+		"{protocol:%v,calledAET:%q,callingAET:%q,appContextName:%q,presContexts:[",
+		pdu.protocol,
+		strings.TrimSpace(pdu.calledAETitle),
+		strings.TrimSpace(pdu.callingAETitle),
+		pdu.appContextName)
+
+	// print the list of presentation contexts
+	for item := pdu.presContexts.Front(); item != nil; item = item.Next() {
+		s += fmt.Sprintf("%s,", item.Value.(*PresContext))
+	}
+
+	// trim the trailing comma separating presentation contexts and add the enclosing bracket
+	s = strings.TrimSuffix(s, ",") + "]"
+
+	// add the user info
+	s += fmt.Sprintf(",userInfo:%v", pdu.userInfo)
+
+	// return the fully constructed string including a closing parenthesis
+	return s + "}"
+}
+
+// String returns a string representation of a UserInfo
+func (presContext *PresContext) String() string {
+	s := fmt.Sprintf(
+		"{id:%d,abstractSyntax:%q,transferSyntaxes:[",
+		presContext.id,
+		presContext.abstractSyntax)
+
+	// print the list of transfer syntaxes
+	for item := presContext.transferSyntaxes.Front(); item != nil; item = item.Next() {
+		transferSyntax := item.Value.(string)
+		s += fmt.Sprintf("%q,", transferSyntax)
+	}
+
+	// trim the trailing comma separating transfer syntaxes and add the enclosing bracket
+	s = strings.TrimSuffix(s, ",") + "]"
+
+	// return the fully constructed including a closing parenthesis for this presentation context
+	return s + "}"
+}
+
+// String returns a string representation of a UserInfo
+func (userInfo *UserInfo) String() string {
+	return fmt.Sprintf(
+		"{maxLenReceived:%v,implClassUID:%q,implVersionName:%q,maxNumOpsInvoked:%v,maxNumOpsPerformed:%v}",
+		userInfo.maxLenReceived,
+		userInfo.implClassUID,
+		userInfo.implVersionName,
+		userInfo.maxNumOpsInvoked,
+		userInfo.maxNumOpsPerformed)
 }
 
 // readAssocRQPDU reads an AssocRQPDU from a reader
@@ -136,7 +167,7 @@ func readAssocRQPDU(reader io.Reader) (*AssocRQPDU, error) {
 
 			limitedReader := io.LimitReader(reader, int64(length))
 
-			pcid, err := readByte(limitedReader)
+			id, err := readByte(limitedReader)
 			if err != nil {
 				return nil, err
 			}
@@ -153,7 +184,7 @@ func readAssocRQPDU(reader io.Reader) (*AssocRQPDU, error) {
 			for {
 
 				// read a sub item
-				itemType, err := readByte(limitedReader)
+				subItemType, err := readByte(limitedReader)
 				if err != nil {
 					if errors.Is(err, io.EOF) {
 						break
@@ -166,7 +197,7 @@ func readAssocRQPDU(reader io.Reader) (*AssocRQPDU, error) {
 					return nil, err
 				}
 
-				if itemType == 0x30 {
+				if subItemType == 0x30 {
 
 					// abstract syntax
 
@@ -180,7 +211,7 @@ func readAssocRQPDU(reader io.Reader) (*AssocRQPDU, error) {
 						return nil, err
 					}
 
-				} else if itemType == 0x40 {
+				} else if subItemType == 0x40 {
 
 					// transfer syntax
 
@@ -198,26 +229,140 @@ func readAssocRQPDU(reader io.Reader) (*AssocRQPDU, error) {
 				} else {
 
 					// unrecognized item
-					return nil, fmt.Errorf("unrecognized sub item type: 0x%2x", itemType)
+					return nil, fmt.Errorf("unrecognized presentation context sub item type: 0x%02X", subItemType)
 
 				}
 
 			}
 
-			presContext := &PresContext{pcid, abstractSyntax, transferSyntaxes}
+			presContext := &PresContext{id, abstractSyntax, transferSyntaxes}
 			presContexts.PushBack(presContext)
 
 		} else if itemType == 0x050 {
 
-			// just skip the bytes for now
-			if err := skipBytes(reader, uint32(length)); err != nil {
-				return nil, err
+			// create a limited reader for the user info
+			limitedReader := io.LimitReader(reader, int64(length))
+
+			// initialze a user info
+			userInfo = &UserInfo{}
+
+			// read the abstract syntax and transfer syntax items
+			for {
+
+				// read a sub item
+				subItemType, err := readByte(limitedReader)
+				if err != nil {
+					if errors.Is(err, io.EOF) {
+						break
+					}
+					return nil, err
+				}
+
+				// skip a byte, as per the standard
+				if err := skipByte(limitedReader); err != nil {
+					return nil, err
+				}
+
+				if subItemType == 0x51 { // maximum length
+
+					// read the length
+					length, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+
+					// check it
+					if length != 0x04 {
+						return nil, fmt.Errorf("expected length to be 0x04, was 0x%04X", length)
+					}
+
+					// read the maximum length received
+					maxLenReceived, err := readLong(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+					userInfo.maxLenReceived = maxLenReceived
+
+				} else if subItemType == 0x52 { // implementation class UID
+
+					// read the length
+					length, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+
+					// read the implementation class UID
+					implClassUID, err := readUID(limitedReader, uint32(length))
+					if err != nil {
+						return nil, err
+					}
+					userInfo.implClassUID = implClassUID
+
+				} else if subItemType == 0x53 { // maximum number operations
+
+					// read the length
+					length, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+
+					// check it
+					if length != 0x04 {
+						return nil, fmt.Errorf("expected length to be 0x04, was 0x%04X", length)
+					}
+
+					// read the maximum number of operations invoked
+					maxNumOpsInvoked, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+					userInfo.maxNumOpsInvoked = maxNumOpsInvoked
+
+					// read the maximum number of operations performed
+					maxNumOpsPerformed, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+					userInfo.maxNumOpsPerformed = maxNumOpsPerformed
+
+				} else if subItemType == 0x55 { // implementation version name
+
+					// read the length
+					length, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+
+					// read the implementation version name
+					implVersionName, err := readText(limitedReader, uint32(length))
+					if err != nil {
+						return nil, err
+					}
+					userInfo.implVersionName = implVersionName
+
+				} else {
+
+					// unrecognized item
+					fmt.Printf("ignoring unrecognized user info sub item type: 0x%02X\n", subItemType)
+
+					// read the length
+					length, err := readShort(limitedReader, binary.BigEndian)
+					if err != nil {
+						return nil, err
+					}
+
+					// skip the bytes
+					if err := skipBytes(limitedReader, uint32(length)); err != nil {
+						return nil, err
+					}
+				}
+
 			}
 
 		} else {
 
 			// unrecognized item
-			return nil, fmt.Errorf("unrecognized item type: 0x%2x", itemType)
+			return nil, fmt.Errorf("unrecognized item type: 0x%02X", itemType)
 		}
 
 	}
