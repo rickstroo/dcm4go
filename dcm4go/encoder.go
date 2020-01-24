94ac096f23d9ac1b65fb4ad861935a6574b6f475
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-01-24 16:45:57 -0500

    Rewrote encoder to mimic decoder and expanded support for additional VRs.

--- dcm4go/encoder.go
+++ dcm4go/encoder.go
@@ -67,7 +67,7 @@ func (encoder *Encoder) writeObjectWithGroupLength(writer io.Writer, group uint1
 func (encoder *Encoder) writeAttribute(writer io.Writer, attribute *Attribute, transferSyntax *TransferSyntax) error {
 
 	// write tag
-	if err := encoder.writeTag(writer, attribute, transferSyntax.byteOrder); err != nil {
+	if err := encoder.writeTag(writer, attribute.tag, transferSyntax.byteOrder); err != nil {
 		return err
 	}
 
@@ -76,30 +76,32 @@ func (encoder *Encoder) writeAttribute(writer io.Writer, attribute *Attribute, t
 		return err
 	}
 
-	// prepare the value
-	length, err := encoder.prepareValue(attribute)
-	if err != nil {
+	// create a buffer to write the value into
+	buf := new(bytes.Buffer)
+
+	// write value into buffer
+	if err := encoder.writeValue(buf, attribute, transferSyntax); err != nil {
 		return err
 	}
 
 	// write length
-	if err := encoder.writeLength(writer, attribute, length, transferSyntax.explicitVR, transferSyntax.byteOrder); err != nil {
+	if err := encoder.writeLength(writer, attribute, uint32(buf.Len()), transferSyntax.explicitVR, transferSyntax.byteOrder); err != nil {
 		return err
 	}
 
-	// write value
-	if err := encoder.writeValue(writer, attribute, transferSyntax.byteOrder); err != nil {
+	// write the value
+	if err := writeBytes(writer, buf.Bytes()); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (encoder *Encoder) writeTag(writer io.Writer, attribute *Attribute, byteOrder binary.ByteOrder) error {
-	if err := writeShort(writer, toGroup(attribute.tag), byteOrder); err != nil {
+func (encoder *Encoder) writeTag(writer io.Writer, tag uint32, byteOrder binary.ByteOrder) error {
+	if err := writeShort(writer, toGroup(tag), byteOrder); err != nil {
 		return err
 	}
-	if err := writeShort(writer, toElement(attribute.tag), byteOrder); err != nil {
+	if err := writeShort(writer, toElement(tag), byteOrder); err != nil {
 		return err
 	}
 	return nil
@@ -138,22 +140,120 @@ func (encoder *Encoder) writeLength(writer io.Writer, attribute *Attribute, leng
 	return nil
 }
 
-func (encoder *Encoder) writeValue(writer io.Writer, attribute *Attribute, byteOrder binary.ByteOrder) error {
-	switch value := attribute.value.(type) {
-	case []uint16:
-		return encoder.writeShorts(writer, value, byteOrder)
-	case []uint32:
-		return encoder.writeLongs(writer, value, byteOrder)
-	case string:
-		switch attribute.vr {
-		case "UI":
-			return writeUID(writer, value)
-		default:
-			return writeText(writer, value)
-		}
-	default:
-		return fmt.Errorf("Encoder.writeValue: not implemented for value %v", attribute.value)
+func (encoder *Encoder) writeValue(writer io.Writer, attribute *Attribute, transferSyntax *TransferSyntax) error {
+
+	switch attribute.vr {
+	// these VRs support multiple text values
+	case "AE", "AS", "CS", "DA", "DS", "DT", "IS", "LO", "PN", "SH", "TM", "UC":
+		return encoder.writePaddedTexts(writer, attribute.value.([]string))
+	case "AT":
+		return encoder.writeTags(writer, attribute.value.([]uint32), transferSyntax.byteOrder)
+	case "FD", "OD":
+		return encoder.writeDoubles(writer, attribute.value.([]float64), transferSyntax.byteOrder)
+	case "FL", "OF":
+		return encoder.writeFloats(writer, attribute.value.([]float32), transferSyntax.byteOrder)
+		// these VRs support single text values
+	case "LT", "ST", "UT", "UR":
+		return encoder.writePaddedText(writer, attribute.value.(string))
+	case "OB", "OL", "OV", "OW", "UN":
+		return encoder.writePixelData(writer, attribute.value, transferSyntax.byteOrder)
+	case "SL", "UL":
+		return encoder.writeLongs(writer, attribute.value.([]uint32), transferSyntax.byteOrder)
+	case "SQ":
+		return encoder.writeSequence(writer, attribute.value.(*Sequence), transferSyntax)
+	case "SS", "US":
+		return encoder.writeShorts(writer, attribute.value.([]uint16), transferSyntax.byteOrder)
+	case "SV", "UV":
+		return encoder.writeVeryLongs(writer, attribute.value.([]uint64), transferSyntax.byteOrder)
+	case "UI":
+		return encoder.writePaddedUIDs(writer, attribute.value.([]string))
 	}
+
+	// hmm, didn't recognize the VR
+	return ErrUnrecognizedVR
+}
+
+func (encoder *Encoder) writePaddedTexts(writer io.Writer, texts []string) error {
+	text := flattenTexts(texts)
+	if err := encoder.writePaddedText(writer, text); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (encoder *Encoder) writePaddedText(writer io.Writer, text string) error {
+	paddedText := padText(text)
+	if err := writeText(writer, paddedText); err != nil {
+		return err
+	}
+	return nil
+}
+
+// flattenTexts converts an array of strings into a single string with separataor character
+func flattenTexts(texts []string) string {
+	t := ""
+	for _, text := range texts {
+		t += text + "\\"
+	}
+	t = strings.TrimSuffix(t, "\\")
+	return t
+}
+
+// padText returns a string that is even length padded with a space if required
+func padText(text string) string {
+	if isOdd(len(text)) {
+		return text + " "
+	}
+	return text
+}
+
+// isOdd returns true if num is odd
+func isOdd(num int) bool {
+	return num&0x01 != 0
+}
+
+func (encoder *Encoder) writePaddedUIDs(writer io.Writer, uids []string) error {
+	uid := flattenUIDs(uids)
+	if err := encoder.writePaddedUID(writer, uid); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (encoder *Encoder) writePaddedUID(writer io.Writer, uid string) error {
+	paddedUID := padUID(uid)
+	if err := writeUID(writer, paddedUID); err != nil {
+		return err
+	}
+	return nil
+}
+
+// flattenUIDs converts an array of strings into a single string with separataor character
+func flattenUIDs(uids []string) string {
+	u := ""
+	for _, uid := range uids {
+		u += uid + "\\"
+	}
+	u = strings.TrimSuffix(u, "\\")
+	return u
+}
+
+// padUID returns a string that is even length padded with a zero byte if required
+func padUID(uid string) string {
+	if isOdd(len(uid)) {
+		return uid + string(byte(0))
+	}
+	return uid
+}
+
+// writes tags
+func (encoder *Encoder) writeTags(writer io.Writer, tags []uint32, byteOrder binary.ByteOrder) error {
+	for _, tag := range tags {
+		if err := encoder.writeTag(writer, tag, byteOrder); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (encoder *Encoder) writeShorts(writer io.Writer, shorts []uint16, byteOrder binary.ByteOrder) error {
@@ -174,48 +274,37 @@ func (encoder *Encoder) writeLongs(writer io.Writer, longs []uint32, byteOrder b
 	return nil
 }
 
-func (encoder *Encoder) prepareValue(attribute *Attribute) (uint32, error) {
-	var length int
-	switch value := attribute.value.(type) {
-	case []uint16:
-		length = len(value) * 2
-	case []uint32:
-		length = len(value) * 4
-	case []float32:
-		length = len(value) * 4
-	case []float64:
-		length = len(value) * 8
-	case []string:
-		switch attribute.vr {
-		case "UI":
-			s := flattenString(value, byte(0x00))
-			length = len(s)
-			attribute.value = s
-		default:
-			s := flattenString(value, byte(' '))
-			length = len(s)
-			attribute.value = s
+func (encoder *Encoder) writeVeryLongs(writer io.Writer, veryLongs []uint64, byteOrder binary.ByteOrder) error {
+	for _, veryLong := range veryLongs {
+		if err := writeVeryLong(writer, veryLong, byteOrder); err != nil {
+			return err
 		}
-	default:
-		return 0, fmt.Errorf("Encoder.prepareValue: not implemented for value %v", attribute.value)
 	}
-
-	return uint32(length), nil
+	return nil
 }
 
-func flattenString(values []string, padding byte) string {
-	s := ""
-	for _, value := range values {
-		s += value + "\\"
+func (encoder *Encoder) writeFloats(writer io.Writer, floats []float32, byteOrder binary.ByteOrder) error {
+	for _, float := range floats {
+		if err := writeFloat(writer, float, byteOrder); err != nil {
+			return err
+		}
 	}
-	s = strings.TrimSuffix(s, "\\")
-	if isOdd(len(s)) {
-		s += string(padding)
-	}
-	return s
+	return nil
 }
 
-// isOdd returns true if num is odd
-func isOdd(num int) bool {
-	return num&0x01 != 0
+func (encoder *Encoder) writeDoubles(writer io.Writer, doubles []float64, byteOrder binary.ByteOrder) error {
+	for _, double := range doubles {
+		if err := writeDouble(writer, double, byteOrder); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (encoder *Encoder) writeSequence(writer io.Writer, sequence *Sequence, transferSyntax *TransferSyntax) error {
+	return fmt.Errorf("encoder.writeSequence not implemented")
+}
+
+func (encoder *Encoder) writePixelData(writer io.Writer, pixelData interface{}, byteOrder binary.ByteOrder) error {
+	return fmt.Errorf("encoder.writePixelData not implemented")
 }
--- dcm4go/write.go
+++ dcm4go/write.go
@@ -3,6 +3,7 @@ package dcm4go
 import (
 	"encoding/binary"
 	"io"
+	"math"
 )
 
 // writes bytes
@@ -37,6 +38,38 @@ func writeShort(writer io.Writer, short uint16, byteOrder binary.ByteOrder) erro
 func writeLong(writer io.Writer, long uint32, byteOrder binary.ByteOrder) error {
 	var buf [4]byte
 	byteOrder.PutUint32(buf[:], long)
+	if err := writeBytes(writer, buf[:]); err != nil {
+		return err
+	}
+	return nil
+}
+
+// writes an unsigned very long
+func writeVeryLong(writer io.Writer, veryLong uint64, byteOrder binary.ByteOrder) error {
+	var buf [8]byte
+	byteOrder.PutUint64(buf[:], veryLong)
+	if err := writeBytes(writer, buf[:]); err != nil {
+		return err
+	}
+	return nil
+}
+
+//return math.Float64frombits(byteOrder.Uint64(buf[:])), nil
+
+// writes a float
+func writeFloat(writer io.Writer, float float32, byteOrder binary.ByteOrder) error {
+	var buf [4]byte
+	byteOrder.PutUint32(buf[:], math.Float32bits(float))
+	if err := writeBytes(writer, buf[:]); err != nil {
+		return err
+	}
+	return nil
+}
+
+// writes a double
+func writeDouble(writer io.Writer, double float64, byteOrder binary.ByteOrder) error {
+	var buf [8]byte
+	byteOrder.PutUint64(buf[:], math.Float64bits(double))
 	if err := writeBytes(writer, buf[:]); err != nil {
 		return err
 	}
