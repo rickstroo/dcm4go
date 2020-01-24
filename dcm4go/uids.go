10d9be6bc3b26894d248b518882472ca200d055d
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-01-24 00:08:46 -0500

    Cleaned up some white space.

--- dcm4go/uids.go
+++ dcm4go/uids.go
@@ -3,15 +3,20 @@ package dcm4go
 const (
 	// ImplicitVRLittleEndianUID is transfer syntax for implicit VR little endian
 	ImplicitVRLittleEndianUID = "1.2.840.10008.1.2"
+
 	// ExplicitVRLittleEndianUID is transfer syntax for explicit VR little endian
 	ExplicitVRLittleEndianUID = "1.2.840.10008.1.2.1"
+
 	// ExplicitVRBigEndianUID is transfer syntax for explicit VR big endian
 	ExplicitVRBigEndianUID = "1.2.840.10008.1.2.2"
+
 	// DeflatedExplicitVRLittleEndianUID is transfer syntax for deflated explicit VR little endian
 	DeflatedExplicitVRLittleEndianUID = "1.2.840.10008.1.2.1.99"
+
 	// JPEG2000ImageCompressionLosslessOnlyUID is transfer syntax uid for JPEG 2000 image compression (lossless only)
 	JPEG2000ImageCompressionLosslessOnlyUID = "1.2.840.10008.1.2.4.90"
-	// JPEG2000ImageCompressUID is transfer syntax uid for JPEG 2000 image compression
+
+	// JPEG2000ImageCompressionUID is transfer syntax uid for JPEG 2000 image compression
 	JPEG2000ImageCompressionUID = "1.2.840.10008.1.2.4.91"
 
 	// VerificationUID is the SOP class UID for verification
@@ -19,6 +24,7 @@ const (
 
 	// EnhancedXAImageStorageUID is the SOP class UID for enhanced XA image storage
 	EnhancedXAImageStorageUID = "1.2.840.10008.5.1.4.1.1.12.1.1"
+
 	// GeneralECGWaveformStorageUID is the SOP class UID for general ECG waveform storage
 	GeneralECGWaveformStorageUID = "1.2.840.10008.5.1.4.1.1.9.1.2"
 )
