36a5e2017a47e44f8fe01d02b9a9b981b3b3375d
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-01-25 10:43:32 -0500

    Added some unit tests for the encoder.

---
+++ dcm4go/encoder_test.go
@@ -0,0 +1,69 @@
+package dcm4go
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+func initEncoderTest() (*Encoder, error) {
+	return newEncoder(), nil
+}
+
+func TestNewEncoder(t *testing.T) {
+	if _, err := initEncoderTest(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestEncodeLongAttributeImplicitVRLittleEndianTS(t *testing.T) {
+	encoder, err := initEncoderTest()
+	if err != nil {
+		t.Error(err)
+	}
+	attribute := &Attribute{FileMetaInformationGroupLengthTag, "UL", []uint32{0x04}}
+	buf := new(bytes.Buffer)
+	if err := encoder.writeAttribute(buf, attribute, ImplicitVRLittleEndianTS); err != nil {
+		t.Error(err)
+	}
+	if err := equal(buf.Bytes(), []byte{2, 0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 0}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestEncodeLongAttributeExplicitVRLittleEndianTS(t *testing.T) {
+	encoder, err := initEncoderTest()
+	if err != nil {
+		t.Error(err)
+	}
+	attribute := &Attribute{FileMetaInformationGroupLengthTag, "UL", []uint32{0x04}}
+	buf := new(bytes.Buffer)
+	if err := encoder.writeAttribute(buf, attribute, ExplicitVRLittleEndianTS); err != nil {
+		t.Error(err)
+	}
+	if err := equal(buf.Bytes(), []byte{2, 0, 0, 0, 85, 76, 4, 0, 4, 0, 0, 0}); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestEncodeLongAttributeExplicitVRBigEndianTS(t *testing.T) {
+	encoder, err := initEncoderTest()
+	if err != nil {
+		t.Error(err)
+	}
+	attribute := &Attribute{FileMetaInformationGroupLengthTag, "UL", []uint32{0x04}}
+	buf := new(bytes.Buffer)
+	if err := encoder.writeAttribute(buf, attribute, ExplicitVRBigEndianTS); err != nil {
+		t.Error(err)
+	}
+	if err := equal(buf.Bytes(), []byte{0, 2, 0, 0, 85, 76, 0, 4, 0, 0, 0, 4}); err != nil {
+		t.Error(err)
+	}
+}
+
+func equal(a, b []byte) error {
+	if !bytes.Equal(a, b) {
+		return fmt.Errorf("while comparing byte streams, found %v while expecting %v", a, b)
+	}
+	return nil
+}
