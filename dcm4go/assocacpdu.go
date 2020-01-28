7982fd4de10a77fe112431d717912d678e887158
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-01-28 14:05:48 -0500

    Replaced the AssocACPDU and AssocRQPDU types with the AssocACRQPDU type.

--- dcm4go/assocacpdu.go
+++ dcm4go/assocacpdu.go
@@ -4,44 +4,37 @@ import (
 	"io"
 )
 
-// AssocACPDU borrows from AssocACRQPDU
-type AssocACPDU struct {
-	AssocACRQPDU
-}
-
-// create an associate acceptance PDU from an associate request PDU
-func newAssocACPDU(assocRQPDU *AssocRQPDU) *AssocACPDU {
-	return &AssocACPDU{
-		AssocACRQPDU{
-			0x01,                       // protocol version, as per the standard
-			assocRQPDU.calledAETitle,   // copy from the request, as per the standard
-			assocRQPDU.callingAETitle,  // copy from the request, as per the standard
-			"1.2.840.10008.3.1.1.1",    // app context name, as per the standard
-			make([]*PresContext, 0, 5), // empty pres context list
-			&UserInfo{
-				16378,             // max length received, need to figure out why dcm4che uses this number
-				"1.2.40.0.13.1.3", // implementation class uid, need to get a root, borrowing dcm4che for now
-				"dcm4go-1.0",      // implementation class name
-				0,                 // max num ops invoked
-				0,                 // max num ops performed
-			},
+// create an associate accept PDU from an associate request PDU
+func newAssocACPDU(assocRQPDU *AssocACRQPDU) *AssocACRQPDU {
+	return &AssocACRQPDU{
+		0x01,                       // protocol version, as per the standard
+		assocRQPDU.calledAETitle,   // copy from the request, as per the standard
+		assocRQPDU.callingAETitle,  // copy from the request, as per the standard
+		"1.2.840.10008.3.1.1.1",    // app context name, as per the standard
+		make([]*PresContext, 0, 5), // empty pres context list
+		&UserInfo{
+			16378,             // max length received, need to figure out why dcm4che uses this number
+			"1.2.40.0.13.1.3", // implementation class uid, need to get a root, borrowing dcm4che for now
+			"dcm4go-1.0",      // implementation class name
+			0,                 // max num ops invoked
+			0,                 // max num ops performed
 		},
 	}
 }
 
-// readAssocACPDU reads an AssocACPDU from a reader
-func readAssocACPDU(reader io.Reader) (*AssocACPDU, error) {
+// readAssocACPDU reads an associate accept
+func readAssocACPDU(reader io.Reader) (*AssocACRQPDU, error) {
 
-	// read the association request
-	assocACRQPDU, err := readAssocACRQPDU(reader, acPresContextItemType)
+	// read the associate request
+	assocACPDU, err := readAssocACRQPDU(reader, acPresContextItemType)
 	if err != nil {
 		return nil, err
 	}
 
 	// construct and return an association request pdu
-	return &AssocACPDU{*assocACRQPDU}, nil
+	return assocACPDU, nil
 }
 
-func writeAssocACPDU(writer io.Writer, assocACPDU *AssocACPDU) error {
-	return writeAssocACRQPDU(writer, &assocACPDU.AssocACRQPDU, aAssociateACPDU, acPresContextItemType)
+func writeAssocACPDU(writer io.Writer, assocACPDU *AssocACRQPDU) error {
+	return writeAssocACRQPDU(writer, assocACPDU, aAssociateACPDU, acPresContextItemType)
 }
--- dcm4go/assocrqpdu.go
+++ dcm4go/assocrqpdu.go
@@ -4,44 +4,38 @@ import (
 	"io"
 )
 
-// AssocRQPDU borrows from the AssocACRQPDU
-type AssocRQPDU struct {
-	AssocACRQPDU
-}
-
 // newAssocRQPDU creates a new association request PDU
-func newAssocRQPDU(calledAETitle string, callingAETitle string, presContexts []*PresContext) *AssocRQPDU {
-	return &AssocRQPDU{
-		AssocACRQPDU{
-			0x01,                    // protocol version, as per the standard
-			calledAETitle,           // title of the called, as per the standard
-			callingAETitle,          // title of the caller, as per the standard
-			"1.2.840.10008.3.1.1.1", // app context name, as per the standard
-			presContexts,            // pres context list
-			&UserInfo{
-				16378,             // max length received, need to figure out why dcm4che uses this number
-				"1.2.40.0.13.1.3", // implementation class uid, need to get a root, borrowing dcm4che for now
-				"dcm4go-1.0",      // implementation class name
-				0,                 // max num ops invoked
-				0,                 // max num ops performed
-			},
+func newAssocRQPDU(calledAETitle string, callingAETitle string, presContexts []*PresContext) *AssocACRQPDU {
+	return &AssocACRQPDU{
+		0x01,                    // protocol version, as per the standard
+		calledAETitle,           // title of the called, as per the standard
+		callingAETitle,          // title of the caller, as per the standard
+		"1.2.840.10008.3.1.1.1", // app context name, as per the standard
+		presContexts,            // pres context list
+		&UserInfo{
+			16378,             // max length received, need to figure out why dcm4che uses this number
+			"1.2.40.0.13.1.3", // implementation class uid, need to get a root, borrowing dcm4che for now
+			"dcm4go-1.0",      // implementation class name
+			0,                 // max num ops invoked
+			0,                 // max num ops performed
 		},
 	}
 }
 
-// readAssocRQPDU reads an AssocRQPDU from a reader
-func readAssocRQPDU(reader io.Reader) (*AssocRQPDU, error) {
+// readAssocRQPDU reads an associate request
+func readAssocRQPDU(reader io.Reader) (*AssocACRQPDU, error) {
 
 	// read the association request
-	assocACRQPDU, err := readAssocACRQPDU(reader, rqPresContextItemType)
+	assocRQPDU, err := readAssocACRQPDU(reader, rqPresContextItemType)
 	if err != nil {
 		return nil, err
 	}
 
 	// construct and return an association request pdu
-	return &AssocRQPDU{*assocACRQPDU}, nil
+	return assocRQPDU, nil
 }
 
-func writeAssocRQPDU(writer io.Writer, assocRQPDU *AssocRQPDU) error {
-	return writeAssocACRQPDU(writer, &assocRQPDU.AssocACRQPDU, aAssociateRQPDU, rqPresContextItemType)
+// writeAssocRQPDU writes an associate request
+func writeAssocRQPDU(writer io.Writer, assocRQPDU *AssocACRQPDU) error {
+	return writeAssocACRQPDU(writer, assocRQPDU, aAssociateRQPDU, rqPresContextItemType)
 }
