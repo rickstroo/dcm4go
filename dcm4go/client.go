42bd9428405c61bb6d48ca9863aeb5cd98c124e4
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-01 16:28:41 -0500

    Moved client implementation of Verify and Send into library.

--- dcm4go/client.go
+++ dcm4go/client.go
@@ -4,6 +4,9 @@ package dcm4go
 
 import (
 	"fmt"
+	"net"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -12,6 +15,10 @@ import (
 //
 // A Client's zero value is a usable client.
 type Client struct {
+
+	// AETitle is the AE Title of the client.
+	// If zero, a value of "DCMSND" will be used when establishing associations
+	AETitle string
 
 	// ConnectTimeout is the maximum time allowed for connection to a server
 	// If zero, there is no connect timeout.
@@ -26,24 +33,224 @@ type Client struct {
 	WriteTimeout time.Duration
 }
 
+func parseAddr(addr string) (string, string, error) {
+	s := strings.Split(addr, "@")
+	if len(s) != 2 {
+		return "", "", fmt.Errorf("expected address of form 'ae@host:port', found '%v'", addr)
+	}
+	return s[0], s[1], nil
+}
+
 // Verify sends  DICOM verifiction request from a client to a server
 func (client *Client) Verify(addr string) error {
-	return fmt.Errorf("Client.Send(): not implemented")
+	return client.verify(addr)
 }
 
 // Verify creates a DICOM client and sends a verification request
+// For greater control, create a client and use Client.Verify().
 func Verify(addr string) error {
 	client := &Client{}
-	return client.Verify(addr)
+	return client.verify(addr)
+}
+
+// verify implements the verification request
+func (client *Client) verify(addr string) error {
+
+	// parse the address
+	serverAETitle, serverHostPort, err := parseAddr(addr)
+	if err != nil {
+		return err
+	}
+
+	// attempt a connection
+	conn, err := net.Dial("tcp", serverHostPort)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("connected to %v from %v\n", conn.RemoteAddr(), conn.LocalAddr())
+
+	// ensure the connection gets closed
+	defer conn.Close()
+
+	// define an application entity for managing dicom connections
+	clientAETitle := client.AETitle
+	if clientAETitle == "" {
+		clientAETitle = "DCMSND"
+	}
+	local := NewAE(clientAETitle)
+
+	// request support for verification
+	local.AddRequestedCapability(VerificationUID, []string{ImplicitVRLittleEndianUID})
+	fmt.Printf("local ae:%v\n", local)
+
+	// define the the remote ae
+	remote := NewAE(serverAETitle)
+	fmt.Printf("remote ae:%v\n", remote)
+
+	// request an association
+	assoc, err := RequestAssoc(conn, local, remote)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("negotiated association from %s to %s\n", local.AETitle(), remote.AETitle())
+
+	// send a verification request
+	if err := assoc.Verify(); err != nil {
+		return err
+	}
+
+	// all is well
+	return nil
 }
 
 // Send sends a DICOM store request from a client to a server.
-func (client *Client) Send(addr string, request *Request) error {
-	return fmt.Errorf("Client.Send(): not implemented")
+func (client *Client) Send(addr string, paths ...string) error {
+	return client.send(addr, paths)
+}
+
+// SendN sends a DICOM store request from a client to a server.
+func (client *Client) SendN(addr string, paths []string) error {
+	return client.send(addr, paths)
 }
 
 // Send creates a DICOM client and sends a store request
-func Send(addr string, request *Request) error {
+// For greater control, create a client and use Client.Send().
+func Send(addr string, paths ...string) error {
 	client := &Client{}
-	return client.Send(addr, request)
+	return client.send(addr, paths)
+}
+
+// SendN creates a DICOM client and sends a store request
+func SendN(addr string, paths []string) error {
+	client := &Client{}
+	return client.send(addr, paths)
+}
+
+// send implements the send request
+func (client *Client) send(addr string, paths []string) error {
+
+	// gather all the abstract syntax and transfer syntaxes required
+	capabilities := make([]*Capability, 0, 5)
+	for _, path := range paths {
+		capability, err := readGroupTwo(path)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("capability is %v\n", capability)
+		capabilities = append(capabilities, capability)
+	}
+	fmt.Printf("capabilities is %v\n", capabilities)
+
+	// parse the address
+	serverAETitle, serverHostPort, err := parseAddr(addr)
+	if err != nil {
+		return err
+	}
+
+	// attempt a connection
+	conn, err := net.Dial("tcp", serverHostPort)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("connected to %v from %v\n", conn.RemoteAddr(), conn.LocalAddr())
+
+	// ensure the connection gets closed
+	defer conn.Close()
+
+	// define an application entity for managing dicom connections
+	clientAETitle := client.AETitle
+	if clientAETitle == "" {
+		clientAETitle = "DCMSND"
+	}
+	local := NewAE(clientAETitle)
+
+	// add the capabilities
+	for _, capability := range capabilities {
+		local.AddRequestedCapability(
+			capability.abstractSyntax,
+			capability.transferSyntaxes,
+		)
+	}
+	fmt.Printf("local ae:%v\n", local)
+
+	// define the the remote ae
+	remote := NewAE(serverAETitle)
+	fmt.Printf("remote ae:%v\n", remote)
+
+	// request an association
+	assoc, err := RequestAssoc(conn, local, remote)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("negotiated association from %s to %s\n", local.AETitle(), remote.AETitle())
+
+	// send each file
+	for _, path := range paths {
+		if err := sendFile(path, assoc); err != nil {
+			return err
+		}
+		fmt.Printf("sent file %q\n", path)
+	}
+
+	// all is well
+	return nil
+}
+
+func readGroupTwo(path string) (*Capability, error) {
+
+	// open the file, which returns a reader, defer a close
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// make sure we close the file upon exit
+	defer file.Close()
+
+	// read the group two attributes
+	groupTwo, err := ReadGroupTwo(file, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	// get the sop class uid of the stored object
+	sopClassUID, err := groupTwo.AsString(MediaStorageSOPClassUIDTag, 0)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("sop class uid is %q\n", sopClassUID)
+
+	// get the transfer syntax used to store the file
+	transferSyntaxUID, err := groupTwo.AsString(TransferSyntaxUIDTag, 0)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("transfer syntax uid is %q\n", transferSyntaxUID)
+
+	// all is well, return the sop class uid and the transfer syntax uid
+	capability := &Capability{
+		abstractSyntax:   sopClassUID,
+		transferSyntaxes: []string{transferSyntaxUID},
+	}
+	return capability, nil
+}
+
+func sendFile(path string, assoc *RequestorAssoc) error {
+
+	// open the file, which returns a reader
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	// make sure we close the file upon exit
+	defer file.Close()
+
+	// send the object
+	if err := assoc.Send(file); err != nil {
+		return err
+	}
+
+	// all is well
+	return nil
 }
--- tools/dcmsnd/dcmsnd.go
+++ tools/dcmsnd/dcmsnd.go
@@ -1,120 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/rickstroo/dcm4go/dcm4go"
 )
 
 // simple error management
 func check(err error) {
 	if err != nil {
-		fmt.Printf("check: %v\n", err)
-		os.Exit(1)
-		//		panic(err)
+		panic(err)
 	}
 }
 
 // the main function
 func main() {
 
-	// set the path to the file
+	// set address of server and paths to file
 	// will parse command line args later
-	//	path := "/Users/Rick/data/dicom/ENHXA.dcm"
-
 	addr := "DCMRCV@localhost:4104"
-	//path := "/Users/Rick/data/dicom/ENHX.dcm"
-	check(dcm4go.Verify(addr))
-	//	check(dcm4go.Send(addr, path))
-}
+	paths := []string{
+		"/Users/Rick/data/dicom/ENHXA.dcm",
+		"/Users/Rick/data/dicom/GENECG.dcm",
+	}
 
-// 	// get the sop class uid and transfer syntax uid
-// 	sopClassUID, transferSyntaxUID, err := readGroupTwo(path)
-// 	check(err)
-//
-// 	// attempt a connection
-// 	conn, err := net.Dial("tcp", "localhost:4104")
-// 	check(err)
-// 	fmt.Printf("connected to %v from %v\n", conn.RemoteAddr(), conn.LocalAddr())
-//
-// 	// ensure the connection gets closed
-// 	defer conn.Close()
-//
-// 	// define an application entity for managing dicom connections
-// 	// request support for verification
-// 	// request support for storage for the type of object that we read
-// 	local := dcm4go.NewAE("DCMSND")
-// 	local.AddRequestedCapability(dcm4go.VerificationUID, []string{dcm4go.ImplicitVRLittleEndianUID})
-// 	local.AddRequestedCapability(sopClassUID, []string{transferSyntaxUID})
-// 	fmt.Printf("local ae:%v\n", local)
-//
-// 	// define the the remote ae
-// 	remote := dcm4go.NewAE("DCMRCV")
-// 	fmt.Printf("remote ae:%v\n", remote)
-//
-// 	// request an association
-// 	assoc, err := dcm4go.RequestAssoc(conn, local, remote)
-// 	fmt.Printf("negotated association from %s to %s\n", local.AETitle(), remote.AETitle())
-// 	check(err)
-//
-// 	// send a verification request
-// 	check(assoc.Verify())
-//
-// 	// send object
-// 	check(send(path, assoc))
-//
-// 	// release association
-// 	check(assoc.RequestRelease())
-// }
-//
-// func readGroupTwo(path string) (string, string, error) {
-//
-// 	// open the file, which returns a reader, defer a close
-// 	file, err := os.Open(path)
-// 	check(err)
-//
-// 	// make sure we close the file upon exit
-// 	defer file.Close()
-//
-// 	// read the group two attributes
-// 	groupTwo, err := dcm4go.ReadGroupTwo(file, 0)
-// 	if err != nil {
-// 		return "", "", err
-// 	}
-//
-// 	// get the sop class uid of the stored object
-// 	sopClassUID, err := groupTwo.AsString(dcm4go.MediaStorageSOPClassUIDTag, 0)
-// 	if err != nil {
-// 		return "", "", err
-// 	}
-// 	fmt.Printf("sop class uid is %q\n", sopClassUID)
-//
-// 	// get the transfer syntax used to store the file
-// 	transferSyntaxUID, err := groupTwo.AsString(dcm4go.TransferSyntaxUIDTag, 0)
-// 	if err != nil {
-// 		return "", "", err
-// 	}
-// 	fmt.Printf("transfer syntax uid is %q\n", transferSyntaxUID)
-//
-// 	// all is well, return the sop class uid and the transfer syntax uid
-// 	return sopClassUID, transferSyntaxUID, nil
-// }
-//
-// func send(path string, assoc *dcm4go.RequestorAssoc) error {
-//
-// 	// open the file, which returns a reader
-// 	file, err := os.Open(path)
-// 	check(err)
-//
-// 	// make sure we close the file upon exit
-// 	defer file.Close()
-//
-// 	// send the object
-// 	if err := assoc.Send(file); err != nil {
-// 		return err
-// 	}
-//
-// 	// all is well
-// 	return nil
-// }
+	// try a verify, then send the files
+	check(dcm4go.Verify(addr))
+	check(dcm4go.SendN(addr, paths))
+}
