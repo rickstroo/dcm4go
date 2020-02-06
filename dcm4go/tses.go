c0fcea726dc6d437b291dc8539caf72f6cd9f649
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-06 13:53:42 -0500

    Added some common UIDs ans transfer syntaxes.

--- dcm4go/tses.go
+++ dcm4go/tses.go
@@ -5,23 +5,72 @@ import "encoding/binary"
 var (
 	// ImplicitVRLittleEndianTS is transfer syntax for implicit VR little endian
 	ImplicitVRLittleEndianTS = &TransferSyntax{ImplicitVRLittleEndianUID, false, binary.LittleEndian, "Implicit VR Little Endian"}
+
 	// ExplicitVRLittleEndianTS is transfer syntax for explicit VR little endian
 	ExplicitVRLittleEndianTS = &TransferSyntax{ExplicitVRLittleEndianUID, true, binary.LittleEndian, "Explicit VR Little Endian"}
+
 	// DeflatedExplicitVRLittleEndianTS is transfer syntax for deflated explicit VR little endian
 	DeflatedExplicitVRLittleEndianTS = &TransferSyntax{DeflatedExplicitVRLittleEndianUID, true, binary.LittleEndian, "Deflated Explicit VR Little Endian"}
+
 	// ExplicitVRBigEndianTS is transfer syntax for explicit VR big endian
 	ExplicitVRBigEndianTS = &TransferSyntax{ExplicitVRBigEndianUID, true, binary.BigEndian, "Explicit VR Big Endian"}
+
+	// JPEGBaselineLossy8BitImageCompressionTS is transfer syntax uid for JPEG lossy 8 bit image compression
+	JPEGBaselineLossy8BitImageCompressionTS = &TransferSyntax{JPEGBaselineLossy8BitImageCompressionUID, true, binary.LittleEndian, "JPEG Baseline Lossy 8 Bit Image Compression"}
+
+	// JPEGBaselineLossy12BitImageCompressionTS is transfer syntax uid for JPEG lossy 12 bit image compression
+	JPEGBaselineLossy12BitImageCompressionTS = &TransferSyntax{JPEGBaselineLossy12BitImageCompressionUID, true, binary.LittleEndian, "JPEG Baseline Lossy 12 Bit Image Compression"}
+
+	// JPEGLosslessNonHierarchicalImageCompressionTS is transfer syntax uid for JPEG lossless non-hierarchical image compression
+	JPEGLosslessNonHierarchicalImageCompressionTS = &TransferSyntax{JPEGLosslessNonHierarchicalImageCompressionUID, true, binary.LittleEndian, "JPEG Lossless Non-Hierarchical Image Compression"}
+
+	// JPEGLSLosslessImageCompressionTS is transfer syntax uid for JPEG-LS lossless image compression
+	JPEGLSLosslessImageCompressionTS = &TransferSyntax{JPEGLSLosslessImageCompressionUID, true, binary.LittleEndian, "JPEG-LS Lossless Image Compression"}
+
+	// JPEGLSLossyImageCompressionTS is transfer syntax uid for JPEG-LS lossy (near lossless) image compression
+	JPEGLSLossyImageCompressionTS = &TransferSyntax{JPEGLSLossyImageCompressionUID, true, binary.LittleEndian, "JPEG-LS Lossy Image Compression"}
+
 	// JPEG2000ImageCompressionLosslessOnlyTS is transfer syntax for JPEG 2000 image compression lossless only
 	JPEG2000ImageCompressionLosslessOnlyTS = &TransferSyntax{JPEG2000ImageCompressionLosslessOnlyUID, true, binary.LittleEndian, "JPEG 2000 Image Compression (Lossless Only)"}
+
 	// JPEG2000ImageCompressionTS is transfer syntax for JPEG 2000 image compression
 	JPEG2000ImageCompressionTS = &TransferSyntax{JPEG2000ImageCompressionUID, true, binary.LittleEndian, "JPEG 2000 Image Compression"}
+
+	// JPEG2000Part2MulticomponentImageCompressionLosslessOnlyTS is transfer syntax uid for JPEG 2000 part 2 multi-component image compression lossless only
+	JPEG2000Part2MulticomponentImageCompressionLosslessOnlyTS = &TransferSyntax{JPEG2000Part2MulticomponentImageCompressionLosslessOnlyUID, true, binary.LittleEndian, "JPEG 2000 Part2 Multi-component Image Compression (Lossless Only)"}
+
+	// JPEG2000Part2MulticomponentImageCompressionTS is transfer syntax uid for JPEG 2000 image compression
+	JPEG2000Part2MulticomponentImageCompressionTS = &TransferSyntax{JPEG2000Part2MulticomponentImageCompressionUID, true, binary.LittleEndian, "JPEG 2000 Part2 Multi-component Image Compression"}
+
+	// JPIPReferencedTS is transfer syntax uid for JPIP referenced
+	JPIPReferencedTS = &TransferSyntax{JPIPReferencedUID, true, binary.LittleEndian, "JPIP Referenced"}
+
+	// JPIPReferencedDeflateTS is transfer syntax uid for JPIP referenced deflate
+	JPIPReferencedDeflateTS = &TransferSyntax{JPIPReferencedDeflateUID, true, binary.LittleEndian, "JPIP Referenced Deflate"}
+
+	// RLELosslessTS is transfer syntax uid for RLE lossless
+	RLELosslessTS = &TransferSyntax{RLELosslessUID, true, binary.LittleEndian, "RLE Loessless"}
+
+	// RFC2557MimeEncapsulationTS is transfer syntax uid for RFC 2557 mime encapsulation
+	RFC2557MimeEncapsulationTS = &TransferSyntax{RFC2557MimeEncapsulationUID, true, binary.LittleEndian, "RFC 2557 Mime Encapsulation"}
 )
 
 var tses = map[string]*TransferSyntax{
-	ImplicitVRLittleEndianUID:               ImplicitVRLittleEndianTS,
-	ExplicitVRLittleEndianUID:               ExplicitVRLittleEndianTS,
-	DeflatedExplicitVRLittleEndianUID:       DeflatedExplicitVRLittleEndianTS,
-	ExplicitVRBigEndianUID:                  ExplicitVRBigEndianTS,
-	JPEG2000ImageCompressionLosslessOnlyUID: JPEG2000ImageCompressionLosslessOnlyTS,
-	JPEG2000ImageCompressionUID:             JPEG2000ImageCompressionTS,
+	ImplicitVRLittleEndianUID:                                  ImplicitVRLittleEndianTS,
+	ExplicitVRLittleEndianUID:                                  ExplicitVRLittleEndianTS,
+	DeflatedExplicitVRLittleEndianUID:                          DeflatedExplicitVRLittleEndianTS,
+	ExplicitVRBigEndianUID:                                     ExplicitVRBigEndianTS,
+	JPEGBaselineLossy8BitImageCompressionUID:                   JPEGBaselineLossy8BitImageCompressionTS,
+	JPEGBaselineLossy12BitImageCompressionUID:                  JPEGBaselineLossy12BitImageCompressionTS,
+	JPEGLosslessNonHierarchicalImageCompressionUID:             JPEGLosslessNonHierarchicalImageCompressionTS,
+	JPEGLSLosslessImageCompressionUID:                          JPEGLSLosslessImageCompressionTS,
+	JPEGLSLossyImageCompressionUID:                             JPEGLSLossyImageCompressionTS,
+	JPEG2000ImageCompressionLosslessOnlyUID:                    JPEG2000ImageCompressionLosslessOnlyTS,
+	JPEG2000ImageCompressionUID:                                JPEG2000ImageCompressionTS,
+	JPEG2000Part2MulticomponentImageCompressionLosslessOnlyUID: JPEG2000Part2MulticomponentImageCompressionLosslessOnlyTS,
+	JPEG2000Part2MulticomponentImageCompressionUID:             JPEG2000Part2MulticomponentImageCompressionTS,
+	JPIPReferencedUID:                                          JPIPReferencedTS,
+	JPIPReferencedDeflateUID:                                   JPIPReferencedDeflateTS,
+	RLELosslessUID:                                             RLELosslessTS,
+	RFC2557MimeEncapsulationUID:                                RFC2557MimeEncapsulationTS,
 }
--- dcm4go/uids.go
+++ dcm4go/uids.go
@@ -1,23 +1,61 @@
 package dcm4go
 
 const (
+
+	// UIDs for Transfer Syntaxes
+
 	// ImplicitVRLittleEndianUID is transfer syntax for implicit VR little endian
 	ImplicitVRLittleEndianUID = "1.2.840.10008.1.2"
 
 	// ExplicitVRLittleEndianUID is transfer syntax for explicit VR little endian
 	ExplicitVRLittleEndianUID = "1.2.840.10008.1.2.1"
 
+	// DeflatedExplicitVRLittleEndianUID is transfer syntax for deflated explicit VR little endian
+	DeflatedExplicitVRLittleEndianUID = "1.2.840.10008.1.2.1.99"
+
 	// ExplicitVRBigEndianUID is transfer syntax for explicit VR big endian
 	ExplicitVRBigEndianUID = "1.2.840.10008.1.2.2"
 
-	// DeflatedExplicitVRLittleEndianUID is transfer syntax for deflated explicit VR little endian
-	DeflatedExplicitVRLittleEndianUID = "1.2.840.10008.1.2.1.99"
+	// JPEGBaselineLossy8BitImageCompressionUID is transfer syntax uid for JPEG lossy 8 bit image compression
+	JPEGBaselineLossy8BitImageCompressionUID = "1.2.840.10008.1.2.4.50"
+
+	// JPEGBaselineLossy12BitImageCompressionUID is transfer syntax uid for JPEG lossy 12 bit image compression
+	JPEGBaselineLossy12BitImageCompressionUID = "1.2.840.10008.1.2.4.51"
+
+	// JPEGLosslessNonHierarchicalImageCompressionUID is transfer syntax uid for JPEG lossless non-hierarchical image compression
+	JPEGLosslessNonHierarchicalImageCompressionUID = "1.2.840.10008.1.2.4.70"
+
+	// JPEGLSLosslessImageCompressionUID is transfer syntax uid for JPEG-LS lossless image compression
+	JPEGLSLosslessImageCompressionUID = "1.2.840.10008.1.2.4.80"
+
+	// JPEGLSLossyImageCompressionUID is transfer syntax uid for JPEG-LS lossy (near lossless) image compression
+	JPEGLSLossyImageCompressionUID = "1.2.840.10008.1.2.4.81"
 
 	// JPEG2000ImageCompressionLosslessOnlyUID is transfer syntax uid for JPEG 2000 image compression (lossless only)
 	JPEG2000ImageCompressionLosslessOnlyUID = "1.2.840.10008.1.2.4.90"
 
 	// JPEG2000ImageCompressionUID is transfer syntax uid for JPEG 2000 image compression
 	JPEG2000ImageCompressionUID = "1.2.840.10008.1.2.4.91"
+
+	// JPEG2000Part2MulticomponentImageCompressionLosslessOnlyUID is transfer syntax uid for JPEG 2000 part 2 multi-component image compression lossless only
+	JPEG2000Part2MulticomponentImageCompressionLosslessOnlyUID = "1.2.840.10008.1.2.4.92"
+
+	// JPEG2000Part2MulticomponentImageCompressionUID is transfer syntax uid for JPEG 2000 image compression
+	JPEG2000Part2MulticomponentImageCompressionUID = "1.2.840.10008.1.2.4.93"
+
+	// JPIPReferencedUID is transfer syntax uid for JPIP referenced
+	JPIPReferencedUID = "1.2.840.10008.1.2.4.94"
+
+	// JPIPReferencedDeflateUID is transfer syntax uid for JPIP referenced deflate
+	JPIPReferencedDeflateUID = "1.2.840.10008.1.2.4.95"
+
+	// RLELosslessUID is transfer syntax uid for RLE lossless
+	RLELosslessUID = "1.2.840.10008.1.2.5"
+
+	// RFC2557MimeEncapsulationUID is transfer syntax uid for RFC 2557 mime encapsulation
+	RFC2557MimeEncapsulationUID = "1.2.840.10008.1.2.6.1"
+
+	// UIDs for Services
 
 	// VerificationUID is the SOP class UID for verification
 	VerificationUID = "1.2.840.10008.1.1"
@@ -27,6 +65,8 @@ const (
 
 	// GeneralECGWaveformStorageUID is the SOP class UID for general ECG waveform storage
 	GeneralECGWaveformStorageUID = "1.2.840.10008.5.1.4.1.1.9.1.2"
+
+	// UIDs for Other Purposes
 
 	// ApplicationContextNameUID is the UID for the Application Context Name
 	ApplicationContextNameUID = "1.2.840.10008.3.1.1.1"
