6e43b9fd3ad9b7af84efabb5d0a16e50f9969551
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-13 11:33:34 -0500

    Added new tool, dcm2txt.

---
+++ tools/dcm2txt/dcm2txt.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/rickstroo/dcm4go/dcm4go"
+)
+
+// simple error management
+func check(err error) {
+	if err != nil {
+		log.Fatalf("error is %v", err)
+	}
+}
+
+// the main function
+func main() {
+
+	var path string
+	var help bool
+
+	flag.StringVar(&path, "path", "", "path to file to parse")
+	flag.BoolVar(&help, "help", false, "display usage")
+
+	flag.Parse()
+
+	if help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	fmt.Printf("path is %q\n", path)
+
+	file, err := os.Open(path)
+	check(err)
+
+	defer file.Close()
+
+	object, err := dcm4go.Parse(file)
+	check(err)
+
+	for _, attr := range object.Attributes() {
+		log.Printf("%v\n", attr)
+	}
+}
