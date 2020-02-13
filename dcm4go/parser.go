7b7913638d846ee086e48a7a4bc22ffda72da316
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-13 17:00:09 -0500

    Need to export Opts from Parser to make it user accessible.

--- dcm4go/parser.go
+++ dcm4go/parser.go
@@ -6,7 +6,7 @@ import (
 
 // A Parser can parse a DICOM object from a reader
 type Parser struct {
-	opts *ParserOpts
+	Opts *ParserOpts
 }
 
 // ParserOpts impact the behaviour of a Parser
@@ -16,7 +16,7 @@ type ParserOpts struct {
 
 // Parse parses a DICOM object from a reader and returns the object
 func (parser *Parser) Parse(reader io.Reader) (*Object, error) {
-	object, err := ReadFile(reader, parser.opts.BulkDataThreshold)
+	object, err := ReadFile(reader, parser.Opts.BulkDataThreshold)
 	if err != nil {
 		return nil, err
 	}
@@ -26,6 +26,6 @@ func (parser *Parser) Parse(reader io.Reader) (*Object, error) {
 // Parse parses a DICOM object from a reader using a default set of options
 func Parse(reader io.Reader) (*Object, error) {
 	opts := &ParserOpts{BulkDataThreshold: 1024}
-	parser := &Parser{opts: opts}
+	parser := &Parser{Opts: opts}
 	return parser.Parse(reader)
 }
