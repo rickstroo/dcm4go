ba66487b93679379e35e2eb539cbe5bcd1e1e0f7
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-13 20:07:40 -0500

    Simplifying sending API.  Model after parsing API.

---
+++ dcm4go/sender.go
@@ -0,0 +1,34 @@
+package dcm4go
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+// A Sender can send a DICOM object to an AE.
+type Sender struct {
+	Opts *SenderOpts
+}
+
+// SenderOpts impact the behaviour of a Sender.
+type SenderOpts struct {
+	ConnectTimeOut time.Duration // a zero value means no connect timeout
+	WriteTimeOut   time.Duration // a zero value means no write timeout
+	ReadTimeOut    time.Duration // a zero value means no read timeout
+}
+
+// Send sends a DICOM object to an AE.
+// The address of the AE is of the format 'aetitle@host:port'.
+func (sender *Sender) Send(reader io.Reader, addr string) error {
+	return fmt.Errorf("Sender.Send(): not implemented")
+}
+
+// Send sends a DICOM object to another AE using a default set of options.
+// To gain more control over the sending, the user should create a Sender
+// with the desired SenderOpts.
+func Send(reader io.Reader, addr string) error {
+	opts := &SenderOpts{}
+	sender := &Sender{Opts: opts}
+	return sender.Send(reader, addr)
+}
--- tools/dcmsnd/dcmsnd.go
+++ tools/dcmsnd/dcmsnd.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/rickstroo/dcm4go/dcm4go"
 )
@@ -13,23 +11,19 @@ import (
 // simple error management
 func check(err error) {
 	if err != nil {
-		log.Printf("error is %v", err)
-		//		panic(err)
-		os.Exit(1)
+		log.Fatalf("error is %v", err)
 	}
 }
 
 // the main function
 func main() {
 
-	var local string
-	var remote string
 	var path string
+	var remote string
 	var help bool
 
-	flag.StringVar(&local, "local", "DCMSND", "AE title for local client")
-	flag.StringVar(&remote, "remote", "DCMRCV@localhost:4104", "AE title, host and port for remote server of form 'ae@host:port'")
-	flag.StringVar(&path, "path", "", "comma separated list of files to send")
+	flag.StringVar(&path, "path", "image.dcm", "specify path of file to send")
+	flag.StringVar(&remote, "remote", "DCMRCV@localhost:4104", "specify ae, host and port in the form 'ae@host:port' of the remote AE")
 	flag.BoolVar(&help, "help", false, "display usage")
 
 	flag.Parse()
@@ -39,40 +33,10 @@ func main() {
 		os.Exit(0)
 	}
 
-	paths := strings.Split(path, ",")
-
-	fmt.Printf("local is %q\n", local)
-	fmt.Printf("remote is %q\n", remote)
-	fmt.Printf("paths is %q\n", paths)
-
-	// create a client
-	client := &dcm4go.Client{
-		AETitle: local,
-	}
-
-	// we're going to use a C-Echo service
-	cEchoService := &dcm4go.CEchoService{}
-
-	// we're also going to use a C-Store service
-	cStoreService := &dcm4go.CStoreService{}
-
-	// discover the capabilities of the files that we are going to send
-	capabilities, err := dcm4go.ReadCapabilities(paths)
+	file, err := os.Open(path)
 	check(err)
-	cStoreService.Capabilities = capabilities
 
-	// connect with the SCUs that we are going to use
-	check(client.Connect(remote, cEchoService, cStoreService))
+	defer file.Close()
 
-	// defer close
-	defer func() {
-		client.Close()
-	}()
-
-	// verify
-	check(client.Verify())
-
-	// send the files
-	check(client.Send(paths))
-
+	check(dcm4go.Send(file, remote))
 }
