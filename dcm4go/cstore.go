c496bd66dd17f4e2d6aa908f0d71cfa133a8fcda
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-17 11:09:05 -0500

    Refactored NewCStoreRequest and NewCStoreResponse.  Will likely retire these soon.

--- dcm4go/cstore.go
+++ dcm4go/cstore.go
@@ -1,41 +1,8 @@
 package dcm4go
 
-const (
-	// LowPriority represents a low priority send
-	LowPriority = 0x0002
-
-	// MedPriority represents a medium priority send
-	MedPriority = 0x0000
-
-	// HighPriority represents a high priority send
-	HighPriority = 0x0001
-)
-
 // NewCStoreResponse constructs a C-Store response message based on the C-Store request message
 func NewCStoreResponse(assoc *Assoc, request *Object) (*Object, error) {
-
-	// use the message id from the request as the message id responded to
-	messageID, err := request.asShort(MessageIDTag, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	// use the affected sop class uid from the request in the response
-	affectedSOPClassUID, err := request.asString(AffectedSOPClassUIDTag, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	// construct a command
-	response := newObject()
-	response.addUID(AffectedSOPClassUIDTag, affectedSOPClassUID)
-	response.addShort(CommandFieldTag, "US", CStoreRSP)
-	response.addShort(MessageIDBeingRespondedToTag, "US", messageID)
-	response.addShort(CommandDataSetTypeTag, "US", 0x0101) // no data
-	response.addShort(StatusTag, "US", 0x00)               // success
-
-	// construct and return a message
-	return response, nil
+	return newCStoreResponse(assoc, request)
 }
 
 // NewCStoreRequest constructs a C-Echo request message
@@ -47,17 +14,11 @@ func NewCStoreRequest(assoc *Assoc, sopClassUID string, sopInstanceUID string, t
 		return nil, nil, err
 	}
 
-	// generate a message id
-	messageID := nextMessageID()
-
 	// construct a request
-	request := newObject()
-	request.addUID(AffectedSOPClassUIDTag, sopClassUID)
-	request.addShort(CommandFieldTag, "US", CStoreRQ)
-	request.addShort(MessageIDTag, "US", messageID)
-	request.addShort(PriorityTag, "US", MedPriority)
-	request.addShort(CommandDataSetTypeTag, "US", 0x0000) // data will follow
-	request.addUID(AffectedSOPInstanceUIDTag, sopInstanceUID)
+	request, err := newCStoreRequest(assoc, sopClassUID, sopInstanceUID)
+	if err != nil {
+		return nil, nil, err
+	}
 
 	// construct and return a message
 	return pc, request, nil
