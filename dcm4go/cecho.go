06b0b36345285b72e69137ec8cab11a9bf7318b8
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-17 11:08:40 -0500

    Refactored NewCEchoRequest and NewCEchoREsponse.  Will probably retire these soon.

--- dcm4go/cecho.go
+++ dcm4go/cecho.go
@@ -2,23 +2,7 @@ package dcm4go
 
 // NewCEchoResponse constructs a C-Echo response message based on the C-Echo request message
 func NewCEchoResponse(assoc *Assoc, request *Object) (*Object, error) {
-
-	// use the message id from the request as the message id responded to
-	messageID, err := request.asShort(MessageIDTag, 0)
-	if err != nil {
-		return nil, err
-	}
-
-	// construct a command
-	response := newObject()
-	response.addUID(AffectedSOPClassUIDTag, VerificationUID)
-	response.addShort(CommandFieldTag, "US", CEchoRSP)
-	response.addShort(MessageIDBeingRespondedToTag, "US", messageID)
-	response.addShort(CommandDataSetTypeTag, "US", 0x0101)
-	response.addShort(StatusTag, "US", 0x00)
-
-	// construct and return a message
-	return response, nil
+	return newCEchoResponse(assoc, request)
 }
 
 // NewCEchoRequest constructs a C-Echo request message
@@ -31,11 +15,10 @@ func NewCEchoRequest(assoc *Assoc) (*PresContext, *Object, error) {
 	}
 
 	// construct a command
-	request := newObject()
-	request.addUID(AffectedSOPClassUIDTag, VerificationUID)
-	request.addShort(CommandFieldTag, "US", CEchoRQ)
-	request.addShort(MessageIDTag, "US", nextMessageID())
-	request.addShort(CommandDataSetTypeTag, "US", 0x0101)
+	request, err := newCEchoRequest(assoc)
+	if err != nil {
+		return nil, nil, err
+	}
 
 	// construct and return a message
 	return pc, request, nil
