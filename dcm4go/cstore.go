1ea23717e5993a809e8d3477ce7c9a9552973f0a
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-02-19 13:44:42 -0500

    Removed obsolete files.

--- dcm4go/cstore.go
+++
@@ -1,3 +0,0 @@
-// Copyright 2020 Rick Stroobosscher.  All rights reserved.
-
-package dcm4go
--- dcm4go/response.go
+++
@@ -1,8 +0,0 @@
-package dcm4go
-
-// A Response represents a response to a DICOM request.
-type Response struct {
-	presContext *PresContext
-	command     *Object
-	data        *Object
-}
