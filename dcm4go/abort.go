2cafcdf77f5eefa04090727294df9a3beedc252a
Rick Stroobosscher <Rick@ricks-mbp.lan>
2020-03-25 13:25:18 -0400

    Commented out a bunch of experimental code that I will likely deprecate.

--- dcm4go/abort.go
+++ dcm4go/abort.go
@@ -5,30 +5,30 @@
 
 package dcm4go
 
-import "io"
-
-// AAbort defines a structure and methods used to send and receive
-// abort request and response messages.
-type AAbort struct {
-	Source byte
-	Reason byte
-}
-
-// Read reads an AAbort from a reader.
-func (aAbort *AAbort) Read(reader io.Reader) error {
-	return nil
-}
-
-// ReadAAbort reads an AAbort from a reader.
-func ReadAAbort(reader io.Reader) (*AAbort, error) {
-	aAbort := &AAbort{}
-	if err := aAbort.Read(reader); err != nil {
-		return nil, err
-	}
-	return aAbort, nil
-}
-
-// Write write an AAbort to a writer.
-func (aAbort *AAbort) Write(writer io.Writer) error {
-	return nil
-}
+// import "io"
+//
+// // AAbort defines a structure and methods used to send and receive
+// // abort request and response messages.
+// type AAbort struct {
+// 	Source byte
+// 	Reason byte
+// }
+//
+// // Read reads an AAbort from a reader.
+// func (aAbort *AAbort) Read(reader io.Reader) error {
+// 	return nil
+// }
+//
+// // ReadAAbort reads an AAbort from a reader.
+// func ReadAAbort(reader io.Reader) (*AAbort, error) {
+// 	aAbort := &AAbort{}
+// 	if err := aAbort.Read(reader); err != nil {
+// 		return nil, err
+// 	}
+// 	return aAbort, nil
+// }
+//
+// // Write write an AAbort to a writer.
+// func (aAbort *AAbort) Write(writer io.Writer) error {
+// 	return nil
+// }
--- dcm4go/pdu0.go
+++ dcm4go/pdu0.go
@@ -5,86 +5,86 @@
 
 package dcm4go
 
-import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-	"io"
-)
-
-// PDU is used to define the structure and methods of a Protocol Data Unit.
-// PDUs are used to encode and exchange messages between application entities.
-type PDU struct {
-	typ byte
-	buf []byte
-}
-
-// ReadPDU reads a PDU
-func ReadPDU(reader io.Reader) (*PDU, error) {
-	typ, err := readByte(reader)
-	if err != nil {
-		return nil, err
-	}
-	if err := skipByte(reader); err != nil {
-		return nil, err
-	}
-	len, err := readLong(reader, binary.BigEndian)
-	if err != nil {
-		return nil, err
-	}
-	buf, err := readBytes(reader, len)
-	if err != nil {
-		return nil, err
-	}
-	pdu := &PDU{
-		typ: typ,
-		buf: buf,
-	}
-	return pdu, nil
-}
-
-// PDUReader creates a reader for a PDU.
-func PDUReader(pdu *PDU) io.Reader {
-	return bytes.NewReader(pdu.buf)
-}
-
-// ParsePDU reads and parses a PDU
-func ParsePDU(reader io.Reader) (interface{}, error) {
-	pdu, err := ReadPDU(reader)
-	if err != nil {
-		return nil, err
-	}
-	pduReader := PDUReader(pdu)
-	switch pdu.typ {
-	case aAbortPDU:
-		return ReadAAbort(pduReader)
-	case aAssociateRQPDU:
-		return ReadAAssociateRQ(pduReader)
-	case aAssociateACPDU:
-		return ReadAAssociateAC(pduReader)
-	case aAssociateRJPDU:
-		return ReadAAssociateRJ(pduReader)
-	case aReleaseRQPDU:
-		return ReadAReleaseRQ(pduReader)
-	case aReleaseRPPDU:
-		return ReadAReleaseRP(pduReader)
-	}
-	return nil, fmt.Errorf("unrecognized PDU type, %v", pdu.typ)
-}
-
-// WritePDU writes a PDU
-func WritePDU(writer io.Writer, pdu *PDU) error {
-	if err := writeByte(writer, pdu.typ); err != nil {
-		return err
-	}
-	if err := writeByte(writer, 0x00); err != nil {
-		return err
-	}
-	if err := writeLong(writer, uint32(len(pdu.buf)), binary.BigEndian); err != nil {
-		return err
-	}
-	if err := writeBytes(writer, pdu.buf); err != nil {
-		return err
-	}
-	return nil
-}
+// import (
+// 	"bytes"
+// 	"encoding/binary"
+// 	"fmt"
+// 	"io"
+// )
+//
+// // PDU is used to define the structure and methods of a Protocol Data Unit.
+// // PDUs are used to encode and exchange messages between application entities.
+// type PDU struct {
+// 	typ byte
+// 	buf []byte
+// }
+//
+// // ReadPDU reads a PDU
+// func ReadPDU(reader io.Reader) (*PDU, error) {
+// 	typ, err := readByte(reader)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if err := skipByte(reader); err != nil {
+// 		return nil, err
+// 	}
+// 	len, err := readLong(reader, binary.BigEndian)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	buf, err := readBytes(reader, len)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	pdu := &PDU{
+// 		typ: typ,
+// 		buf: buf,
+// 	}
+// 	return pdu, nil
+// }
+//
+// // PDUReader creates a reader for a PDU.
+// func PDUReader(pdu *PDU) io.Reader {
+// 	return bytes.NewReader(pdu.buf)
+// }
+//
+// // ParsePDU reads and parses a PDU
+// func ParsePDU(reader io.Reader) (interface{}, error) {
+// 	pdu, err := ReadPDU(reader)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	pduReader := PDUReader(pdu)
+// 	switch pdu.typ {
+// 	case aAbortPDU:
+// 		return ReadAAbort(pduReader)
+// 	case aAssociateRQPDU:
+// 		return ReadAAssociateRQ(pduReader)
+// 	case aAssociateACPDU:
+// 		return ReadAAssociateAC(pduReader)
+// 	case aAssociateRJPDU:
+// 		return ReadAAssociateRJ(pduReader)
+// 	case aReleaseRQPDU:
+// 		return ReadAReleaseRQ(pduReader)
+// 	case aReleaseRPPDU:
+// 		return ReadAReleaseRP(pduReader)
+// 	}
+// 	return nil, fmt.Errorf("unrecognized PDU type, %v", pdu.typ)
+// }
+//
+// // WritePDU writes a PDU
+// func WritePDU(writer io.Writer, pdu *PDU) error {
+// 	if err := writeByte(writer, pdu.typ); err != nil {
+// 		return err
+// 	}
+// 	if err := writeByte(writer, 0x00); err != nil {
+// 		return err
+// 	}
+// 	if err := writeLong(writer, uint32(len(pdu.buf)), binary.BigEndian); err != nil {
+// 		return err
+// 	}
+// 	if err := writeBytes(writer, pdu.buf); err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
